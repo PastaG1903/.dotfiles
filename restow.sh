9e20ea6f7843267c4c2550315906316118701dbf
PastaG1903 <mallito1903@hotmail.com>
2025-05-18 09:58:56 -0600

    config reload

---
+++ restow.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+list=""
+for elem in $(lsd -1FX | grep /); do
+	if [ "$elem" != "grub/" ] && [ "$elem" != "keyd/" ]; then
+		list="$list ${elem::-1}"
+	fi
+done
+
+for elem in $list; do
+	stow "$elem"
+done
+
+sudo rm /etc/default/grub
+sudo stow --target=/etc/default/ grub
+
+sudo stow --target=/etc/ keyd
