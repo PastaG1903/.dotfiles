90b22f7986d192f4bab9e6f0ad983aa0e3933591
PastaG1903 <mallito1903@hotmail.com>
2025-05-22 22:42:22 -0600

    config reload

--- bashrc/.scripts/iwdtui.sh
+++ bashrc/.scripts/iwdtui.sh
@@ -3,3 +3,11 @@
 options=$(printf "Add\nRemove\nList")
 option=$(gum choose $options)
 
+if [ "$option" = "Add" ];
+  devices=$(printf "station list" | iwctl | grep con)
+  devs=""
+  for elem in $devices; do
+    devs=$devs" $(echo $elem | awk '{print $2}')"
+  done
+  device=$(gum choose $devs)
+fi
