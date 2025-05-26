e28db79519159311100b08b407460f079cf568dc
PastaG1903 <mallito1903@hotmail.com>
2025-05-26 15:28:04 -0600

    config reload

--- bashrc/.scripts/syssertui.sh
+++ bashrc/.scripts/syssertui.sh
@@ -2,21 +2,24 @@
 
 while true; do
   services=$(systemctl list-unit-files | grep service | grep -v systemd | grep -v dbus | awk '{print $1}' | sed 's/\.[^.]*$//')
+  services=$(printf "$services\nExit")
   service=$(gum filter $services --header "Choose the service to manage or select 'Exit' to exit: ")
   if [ "$service" = "" ] || [ "$service" = "Exit" ]; then
     break
   fi
-  isactive=$(systemctl status $service | grep Active | awk '{print $2}')
-  isenabled=$(systemctl list-unit-files | grep $service | awk '{print $2}')
+  isactive=$(systemctl status $service".service" | grep Active | awk '{print $2}')
+  isenabled=$(systemctl list-unit-files | grep $service".service" | awk '{print $2}')
   printf "\n$service is $isenabled and $isactive\n\n"
   actions=$(printf "enable\nstart\nrestart\nreload\ndisable\nstop\n")
   action=$(gum choose $actions --header "What do you want to do to $service? Note: 'reload' is to reload configs")
-  if [ "$action" = "" ]; then
+  while true; do
+    if [ "$action" = "" ]; then
+      break
+    fi
+    sudo systemctl $action $service
+    isactive=$(systemctl status $service".service" | grep Active | awk '{print $2}')
+    isenabled=$(systemctl list-unit-files | grep $service".service" | awk '{print $2}')
+    printf "$service is now $isenabled and $isactive\n"
     break
-  fi
-  sudo systemctl $action $service
-  isactive=$(systemctl status $service | grep Active | awk '{print $2}')
-  isenabled=$(systemctl list-unit-files | grep $service | awk '{print $2}')
-  printf "$service is now $isenabled and $isactive\n"
-
+  done
 done
