8881ced5559adcbaef7d101bade777deb3adf5c7
PastaG1903 <mallito1903@hotmail.com>
2025-05-26 12:35:21 -0600

    config reload

--- bashrc/.scripts/syssertui.sh
+++ bashrc/.scripts/syssertui.sh
@@ -9,7 +9,7 @@ while true; do
   isactive=$(systemctl status $service | grep Active | awk '{print $2}')
   isenabled=$(systemctl list-unit-files | grep $service | awk '{print $2}')
   printf "\n$service is $isenabled and $isactive\n\n"
-  actions=$(printf "enable\nstart\ndisable\nstop\n")
+  actions=$(printf "enable\nstart\nrestart\ndisable\nstop\n")
   action=$(gum choose $actions --header "What do you want to do to $service?")
   if [ "$action" = "" ]; then
     break
