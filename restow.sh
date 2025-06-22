e6a67469818602ac944601b62f22a03d8aa6123c
PastaG1903 <mallito1903@hotmail.com>
2025-06-21 18:44:17 -0600

    Config Reload

--- restow.sh
+++ restow.sh
@@ -11,10 +11,10 @@ for elem in $list; do
 	stow "$elem"
 done
 
-#if [ "$@" = "--remove" ]; then
-	#sudo rm /etc/default/grub
-#fi
+if [ "$@" = "-rgrub" ]; then
+	sudo rm /etc/default/grub
+	sudo stow --target=/etc/default/ grub
+fi
 
-sudo stow --target=/etc/default/ grub
-
+sudo rm /etc/keyd/
 sudo stow --target=/etc/ keyd
