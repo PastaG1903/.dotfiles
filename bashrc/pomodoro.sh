f5b414e70756ff1bb1d28a4b2c69e8395b1c2c8e
PastaG1903 <mallito1903@hotmail.com>
2025-07-10 10:42:43 -0600

    Config Reload

--- bashrc/pomodoro.sh
+++ bashrc/pomodoro.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 while true; do
-    notify-send -t 20000 -A Start=Start "Aight, to start working, press Start." | read input
+    notify-send --wait --expire-time 20000 -A Start=Start "Aight, to start working, press Start." | read input
     cycles=$(($1-1))
     for cycle in $(seq 1 $cycles); do
         sleep $((25*60))
-        notify-send -t 15000 "Time for a break."
+        echo \\a && notify-send -t 15000 "Time for a break."
         sleep $((5*60))
-        notify-send -t 20000 -A Continue=Continue "Break time is up! Let's continue with the next pomodoro." | read input
+        echo \\a && notify-send -t 20000 -A Continue=Continue "Break time is up! Let's continue with the next pomodoro." | read input
     done
     sleep $((25*60))
-    notify-send -t 20000 "Good! Let's take a longer break this time."
+    echo \\a && notify-send -t 20000 "Good! Let's take a longer break this time."
     sleep $(($cycles*5*60))
-    notify-send -t 20000 -A Restart=Restart -A Stop=Stop "Break is over! If you want to keep working, press Restart. Otherwise, press Stop." | read input
+    echo \\a && notify-send -t 20000 -A Restart=Restart -A Stop=Stop "Break is over! If you want to keep working, press Restart. Otherwise, press Stop." | read input
     if [ "$input" = "Stop" ]; then
         break
     fi
