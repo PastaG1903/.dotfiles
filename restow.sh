3a62e632298e2b9361a3f3995ca36c15a6cf182c
PastaG1903 <mallito1903@hotmail.com>
2025-11-11 21:03:45 -0600

    Added pacman hook for cache cleanse

--- restow.sh
+++ restow.sh
@@ -18,6 +18,7 @@ if [ "$@" = "-nohome" ]; then
 	sudo cp ./thinkfan/thinkfan.yaml /etc/thinkfan.yaml
 	sudo cp ./tlp/tlp.conf /etc/tlp.conf
 	sudo cp ./pacman/pacman.conf /etc/pacman.conf
+	sudo cp -p ./pacman/paccache-hook.hook /etc/pacman.d/hooks/
 	sudo cp ./grub-theme-config.txt /boot/grub/themes/tela/theme.txt
 fi
 
